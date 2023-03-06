b193a5f0fc2e891d3a2f2e39f45d24bb33c6f224
Simone Briffa <simone.briffa@magma.it>
2023-03-06 18:57:18 +0100

    first commit

---
+++ src/main/java/com/simonebriffa/skysoft/SkysoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simonebriffa.skysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkysoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkysoftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simonebriffa/skysoft/SkysoftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonebriffa.skysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkysoftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
